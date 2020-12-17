4068b2f647aa354b65e16c463e7ca77e1d80b846
Brandon Plank <brandonleeplank1126@gmail.com>
2020-12-17 16:29:46 -0500

    Push part one

---
+++ SwiftGit2/SwiftGit3-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SwiftGit3-Bridging-Header.h
+//  SwiftGit2
+//
+//  Created by Brandon Plank on 12/17/20.
+//  Copyright © 2020 GitHub, Inc. All rights reserved.
+//
+
+#ifndef SwiftGit3_Bridging_Header_h
+#define SwiftGit3_Bridging_Header_h
+
+#include "push.h"
+
+
+#endif /* SwiftGit3_Bridging_Header_h */
