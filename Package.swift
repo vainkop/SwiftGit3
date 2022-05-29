fc7b5561d606e87f329cba5abbd334fb02e49ba7
Joe Hinkle <joehinkle11@gmail.com>
2022-05-29 03:30:45 -0500

    xc framework for binary dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,7 +22,7 @@ let package = Package(
 	targets: [
 		.target(
 			name: "SwiftGit2",
-			dependencies: ["Clibgit2"],
+			dependencies: ["Clibgit2", "Clibssh2-ios", "Clibcrypto", "Clibssl"],
 			path: "SwiftGit2",
 			exclude: ["Info.plist"]
 		),
@@ -30,6 +30,18 @@ let package = Package(
 			name: "Clibgit2",
 			path: "External/Clibgit2.xcframework"
 		),
+        .binaryTarget(
+            name: "Clibssh2-ios",
+            path: "External/Clibssh2-ios.xcframework"
+        ),
+        .binaryTarget(
+            name: "Clibcrypto",
+            path: "External/Clibcrypto.xcframework"
+        ),
+        .binaryTarget(
+            name: "Clibssl",
+            path: "External/Clibssl.xcframework"
+        ),
 		.testTarget(
 			name: "SwiftGit2Tests",
 			dependencies: ["SwiftGit2", "Quick", "Nimble", "Zip"],
